e00e612aec4d9be47e99cc4a88e05b1e21cd0c5a
Amores03 <carlos.amores17@unach.mx>
2025-02-12 21:59:38 -0600

    primer commit

---
+++ SO/practica1/servicioCPU/servicioCPU/Program.cs
@@ -0,0 +1,7 @@
+using servicioCPU;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+    .Build();
+
+await host.RunAsync();
